3ed07a896dc98e118c3afea92edae5f43b45752f
Nick <oomsnikita@gmail.com>
2021-03-19 14:23:23 +0100

    negative margin taken out. problem with right field fixed

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,6 @@ var mixer = mixitup('.gallery__items');
 var mySwiper = new Swiper('.blog__container', {
   // Optional parameters
   slidesPerView: 1,
-  spaceBetween: 30,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
