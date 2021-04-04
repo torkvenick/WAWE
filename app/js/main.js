3b04f27adb1b10615e847a4f4713f5717265d587
Nick <oomsnikita@gmail.com>
2021-04-04 23:52:10 +0200

    section blog all finished

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,14 @@ var mixer = mixitup('.gallery__items');
 var mySwiper = new Swiper('.blog__container', {
   // Optional parameters
   slidesPerView: 1,
+  loop: true,
+  spaceBetween: 30,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+  },
+  keyboard: {
+    enabled: true,
   },
 });
 
